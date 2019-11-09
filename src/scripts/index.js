acb72d0404ba670974d70ab57a4ed8df7a28d9fe
smartikweb <smartikweb@gmail.com>
2019-11-09 09:25:19 +0200

    Added JS01

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,16 @@
 import "../styles/index.scss";
-//alert("webpack starterkit");
-console.log("Hello World!");
-const a = 5;
-const b = 7;
-console.warn("Wow a + b = ", a + b);
-//alert("123");
+// //alert("webpack starterkit");
+const a = 10;
+const b = 5;
+
+console.log("Переменая a = ", a);
+console.log("Переменная b = ", b);
+console.log("*****");
+console.log("Сумма a + b = ", a + b);
+console.log("Разница a - b = ", a - b);
+console.log("Произедение a * b = ", a * b);
+console.log("Чвчтное a /b = ", a / b);
+// console.warn("Wow a + b = ", a + b);
+// //alert("123");
+// const n = 5.5;
+// console.log (n);
